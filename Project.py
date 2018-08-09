de0890914d1cbf2f2c1fef8c78236c9f1c63695c
Dmitry <42120791+dmitryvolgin@users.noreply.github.com>
2018-08-09 08:43:48 +0300

    Uploaded working file
    Just to start with

---
+++ Project.py
@@ -0,0 +1,18 @@
+print ("Welcome to point cloud rotator.")
+print ("I can make a new text file with coordinates of rotated point cloud or generate file with rotation matrix for CloudCompare.")
+print ("First of all type in Euler coordinates you are given.")
+
+alpha = input ("Alpha: ")
+beta = input ("Beta: ")
+gamma = input ("Gamma: ")
+
+while true:
+	print ("Do you want to generate rotation matrix (G) or rotate the cloud (R)?")
+	choise = input ("Type G or R: ")
+
+	if choise == "G" or choise == "g":
+		print ("Generating")
+	elif choise == "R" or choise == "r":
+		print ("Rotating")
+	else:
+		print ("Incorrect input")
